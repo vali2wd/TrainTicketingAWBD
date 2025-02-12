f0481cf588aa4bb3a6d950914e811a6261ac976c
vali2wd <vali_nafo@yahoo.com>
2025-02-12 12:51:05 +0200

    Get For Routes. Done the record dto.  Added swagger ui.

---
+++ TrainTicketing.Api/Endpoints/Routes/RoutesEndpoints.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainTicketing.Database;
+using TrainTicketing.ValueObjects;
+
+namespace TrainTicketing.Api.Endpoints.Routes;
+
+public static class RoutesEndpoints
+{
+    public static void AddRouteEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/routes", async (TrainTicketingDbContext dbContext) =>
+        {
+            var routes = await dbContext
+                            .Routes
+                            .AsNoTracking()
+                            .Select(r => new RouteWithNameAndImageDto(r.RouteId, r.RouteName, r.ImagePath))
+                            .ToListAsync();
+
+            return Results.Ok(routes);
+        });
+    }
+}
---
+++ TrainTicketing/ValueObjects/RouteWithNameAndImageDto.cs
@@ -0,0 +1,4 @@
+﻿namespace TrainTicketing.ValueObjects;
+
+public record RouteWithNameAndImageDto(Guid RouteId, string RouteName, string ImagePath);
+
