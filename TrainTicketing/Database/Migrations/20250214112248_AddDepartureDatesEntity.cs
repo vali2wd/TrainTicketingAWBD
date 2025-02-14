682bdbcb9c8155a50a6e081ccebd70418c6cb871
vali2wd <vali_nafo@yahoo.com>
2025-02-14 13:23:10 +0200

    Add departureDates Entity and migration

---
+++ TrainTicketing/Database/Migrations/20250214112248_AddDepartureDatesEntity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TrainTicketing.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartureDatesEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DepartureDates",
+                columns: table => new
+                {
+                    DepartureDateId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOfDeparture = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DepartureId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartureDates", x => x.DepartureDateId);
+                    table.ForeignKey(
+                        name: "FK_DepartureDates_Departures_DepartureId",
+                        column: x => x.DepartureId,
+                        principalTable: "Departures",
+                        principalColumn: "DepartureId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.InsertData(
+                table: "DepartureDates",
+                columns: new[] { "DepartureDateId", "DateOfDeparture", "DepartureId" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2025, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 },
+                    { 2, new DateTime(2025, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 },
+                    { 3, new DateTime(2025, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 3 },
+                    { 4, new DateTime(2025, 12, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 4 },
+                    { 5, new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 },
+                    { 6, new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 },
+                    { 7, new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 3 },
+                    { 8, new DateTime(2025, 12, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), 4 },
+                    { 9, new DateTime(2025, 12, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 },
+                    { 10, new DateTime(2025, 12, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 },
+                    { 11, new DateTime(2025, 12, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), 3 },
+                    { 12, new DateTime(2025, 12, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), 4 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartureDates_DepartureId",
+                table: "DepartureDates",
+                column: "DepartureId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DepartureDates");
+        }
+    }
+}
---
+++ TrainTicketing/Entities/DepartureDates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace TrainTicketing.Entities;
+public class DepartureDates
+{
+    public int DepartureDateId { get; set; }
+
+    public DateTime DateOfDeparture { get; set; }
+
+    public int DepartureId { get; set; }
+
+    public Departure Departure { get; set; } = null!;
+}
+
+public class DepartureDatesConfigurator : IEntityTypeConfiguration<DepartureDates>
+{
+    public void Configure(EntityTypeBuilder<DepartureDates> builder)
+    {
+        builder.HasKey(dd => dd.DepartureDateId);
+        builder.HasOne(dd => dd.Departure)
+            .WithMany()
+            .HasForeignKey(dd => dd.DepartureId)
+            .OnDelete(DeleteBehavior.Restrict);
+        builder.HasData(
+            new DepartureDates { DepartureDateId = 1, DateOfDeparture = new DateTime(2025, 12, 1), DepartureId = 1 },
+            new DepartureDates { DepartureDateId = 2, DateOfDeparture = new DateTime(2025, 12, 1), DepartureId = 2 },
+            new DepartureDates { DepartureDateId = 3, DateOfDeparture = new DateTime(2025, 12, 1), DepartureId = 3 },
+            new DepartureDates { DepartureDateId = 4, DateOfDeparture = new DateTime(2025, 12, 1), DepartureId = 4 },
+            new DepartureDates { DepartureDateId = 5, DateOfDeparture = new DateTime(2025, 12, 2), DepartureId = 1 },
+            new DepartureDates { DepartureDateId = 6, DateOfDeparture = new DateTime(2025, 12, 2), DepartureId = 2 },
+            new DepartureDates { DepartureDateId = 7, DateOfDeparture = new DateTime(2025, 12, 2), DepartureId = 3 },
+            new DepartureDates { DepartureDateId = 8, DateOfDeparture = new DateTime(2025, 12, 2), DepartureId = 4 },
+            new DepartureDates { DepartureDateId = 9, DateOfDeparture = new DateTime(2025, 12, 3), DepartureId = 1 },
+            new DepartureDates { DepartureDateId = 10, DateOfDeparture = new DateTime(2025, 12, 3), DepartureId = 2 },
+            new DepartureDates { DepartureDateId = 11, DateOfDeparture = new DateTime(2025, 12, 3), DepartureId = 3 },
+            new DepartureDates { DepartureDateId = 12, DateOfDeparture = new DateTime(2025, 12, 3), DepartureId = 4 }
+            );
+    }
+}
