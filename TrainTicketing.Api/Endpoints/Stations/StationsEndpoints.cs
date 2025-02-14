6e67d17ba9abad0750c37f6b32c778ab804f6a9d
vali2wd <vali_nafo@yahoo.com>
2025-02-14 18:35:54 +0200

    Stations Endpoints

---
+++ TrainTicketing.Api/Endpoints/Stations/StationsEndpoints.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TrainTicketing.Database;
+
+namespace TrainTicketing.Api.Endpoints.Stations;
+
+public static class StationsEndpoints
+{
+    public static void AddStationsEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/stations", async (TrainTicketingDbContext dbContext, CancellationToken ctx) => {
+            var stations = await dbContext.Stations.ToListAsync(ctx);
+        });
+    }
+}
