317a34e3a12d4638e4200f308db30b283e4fd69c
vali2wd <vali_nafo@yahoo.com>
2025-02-14 18:24:56 +0200

    More Logging in departure planning job.
    Log Information at departure planning job on success of saving routes with future date or if throw by means of cancellation token.

--- TrainTicketing.Api/HostedServices/DeparturePlanningJob.cs
+++ TrainTicketing.Api/HostedServices/DeparturePlanningJob.cs
@@ -20,14 +20,15 @@ public class DeparturePlanningJob : BackgroundService
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         using PeriodicTimer timer = new PeriodicTimer(TimeSpan.FromSeconds(15));
+        _logger.LogInformation("Departure Planning Job is starting with time period of {0}", timer.Period);
         while (!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
         {
-            await AddAllDepartures(7, stoppingToken);
+            await this.AddAllDepartures(7, stoppingToken);
         }
     }
 
     public async Task AddAllDepartures(int daysFromToday, CancellationToken cancellationToken)
-    {
+    {   
         using var scope = this.Services.CreateScope();
 
         var dbContext = scope.ServiceProvider.GetRequiredService<TrainTicketingDbContext>();
@@ -35,6 +36,8 @@ public class DeparturePlanningJob : BackgroundService
         var departureIds = await dbContext.Departures.Select(d => d.DepartureId).ToListAsync();
 
         var futureDate = DateTime.UtcNow.AddDays(daysFromToday);
+
+        _logger.LogInformation("Identified {0} routes. Begin to add them with {1}.", departureIds.Count, futureDate);
 
         foreach (var departureId in departureIds)
         {
@@ -44,11 +47,19 @@ public class DeparturePlanningJob : BackgroundService
                 DepartureId = departureId
             });
         }
-        _logger.LogInformation("Added departure dates for {0} departures", departureIds.Count);
-        //await dbContext.SaveChangesAsync(cancellationToken);
+        try
+        {
+            //await dbContext.SaveChangesAsync(cancellationToken);
+            _logger.LogInformation("Successfully added {0} departures with date {1}", departureIds.Count, futureDate);
+        }
+        catch (OperationCanceledException oce)
+        {
+            _logger.LogInformation("Could not seed {0} departures with date {1}. {2}", departureIds.Count, futureDate, oce.Message);
+        }
     }
     public override async Task StopAsync(CancellationToken stoppingToken)
     {
+        _logger.LogInformation("Departure Planning Job is stopping.");
         await base.StopAsync(stoppingToken);
     }
 }
