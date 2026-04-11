eb0389a85ecc9ffa315b9882870ddc7b40c28c01
vali2wd <vali_nafo@yahoo.com>
2026-04-11 19:22:25 +0300

    Added paginated endpoint for stations. Tested endpoint.

--- TrainTicketing.Api/Endpoints/Stations/StationsEndpoints.cs
+++ TrainTicketing.Api/Endpoints/Stations/StationsEndpoints.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TrainTicketing.Contracts.DataTransfer;
 using TrainTicketing.Database;
+using TrainTicketing.DomainModel.Entities;
 
 namespace TrainTicketing.Api.Endpoints.Stations;
 
@@ -12,6 +14,23 @@ public static class StationsEndpoints
             var stations = (await dbContext.Stations.ToListAsync(ctx)).Select(s => new { stationId = s.StationId, stationName = s.StationName });
 
             return Results.Ok(stations);
+        });
+
+        app.MapGet("/stationsPg", async (
+            TrainTicketingDbContext dbContext,
+            CancellationToken ctx,
+            int pageNumber = 1,
+            int pageSize = 10) =>
+        {
+            var stations = (await dbContext
+                                .Stations
+                                .Skip((pageNumber - 1) * pageSize)
+                                .Take(pageSize)
+                                .ToListAsync(ctx))
+                                .Select(s => new { stationId = s.StationId, stationName = s.StationName });
+
+            PaginationResponse<dynamic> response = new(stations, stations.Count(), pageNumber, pageSize);
+            return Results.Ok(response);
         });
     }
 }
