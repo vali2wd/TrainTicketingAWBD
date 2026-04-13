17a4dd97c6cb85f3a381658004b3054693d2d89b
vali2wd <vali_nafo@yahoo.com>
2026-04-13 18:09:36 +0300

    Configured the fake smtp server for netDumbster.

--- TrainTicketing.Notification.Api/Program.cs
+++ TrainTicketing.Notification.Api/Program.cs
@@ -1,7 +1,13 @@
+using netDumbster.smtp;
 using TrainTicketing.Contracts.DataTransfer;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var fakeSmtpServer = SimpleSmtpServer.Start(2525);
+builder.Services.AddSingleton(fakeSmtpServer);
+
 var app = builder.Build();
+
 
 app.MapPost("/emails/send", async (SendEmailRequest request, ILogger<Program> logger) =>
 {
