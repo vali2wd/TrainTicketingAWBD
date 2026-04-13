bb161ab2be4da0c56c0ee78bfa4f703073b46809
vali2wd <vali_nafo@yahoo.com>
2026-04-13 18:12:46 +0300

    Added 2 endpoints for mail faking - one for mail sending, and one for the fake inbox created at netDumbster.

--- TrainTicketing.Notification.Api/Program.cs
+++ TrainTicketing.Notification.Api/Program.cs
@@ -1,4 +1,5 @@
 using netDumbster.smtp;
+using System.Net.Mail;
 using TrainTicketing.Contracts.DataTransfer;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,9 +9,32 @@ builder.Services.AddSingleton(fakeSmtpServer);
 
 var app = builder.Build();
 
-
-app.MapPost("/emails/send", async (SendEmailRequest request, ILogger<Program> logger) =>
+app.MapPost("/emails/send", async (SendEmailRequest request, SimpleSmtpServer smtpServer, ILogger<Program> logger) =>
 {
+    logger.LogInformation(">>> Receiving request for Reservation: {Id}", request.ReservationId);
+
+    using var client = new SmtpClient("localhost", 2525);
+    var mailMessage = new MailMessage("system@trainticketing.com", request.RecipientEmail)
+    {
+        Subject = request.Subject,
+        Body = request.Body
+    };
+
+    await client.SendMailAsync(mailMessage);
+    return Results.Ok(new { status = "Sent to Internal SMTP Server" });
+});
+
+
+app.MapGet("/emails/inbox", (SimpleSmtpServer smtpServer) =>
+{
+    // This looks at the memory of netDumbster
+    var received = smtpServer.ReceivedEmail.Select(e => new {
+        To = e.ToAddresses,
+        Subject = e.Subject,
+        Body = e.MessageParts,
+    });
+
+    return Results.Ok(received);
 });
 
 app.Run();
