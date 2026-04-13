8078fcf7803736c44870946fe9673dbd25f8263b
vali2wd <vali_nafo@yahoo.com>
2026-04-13 18:49:29 +0300

    Added DTO for reservation report.

---
+++ TrainTicketing.Contracts/DataTransfer/ReservationReportDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TrainTicketing.Contracts.DataTransfer;
+
+public record ReservationReportDto(
+    Guid ReservationId,
+    string TrainName,
+    decimal Price,
+    DateTime ReservationDate,
+    string DepartureStation
+);
