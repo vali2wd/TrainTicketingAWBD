711fde85bd0eea8d49966ef603d82da4aead8a53
vali2wd <vali_nafo@yahoo.com>
2026-04-13 17:57:52 +0300

    Added Email Request record.

---
+++ TrainTicketing.Contracts/DataTransfer/SendEmailRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TrainTicketing.Contracts.DataTransfer;
+
+public record SendEmailRequest(
+    string RecipientEmail,
+    string Subject,
+    string Body,
+    Guid ReservationId
+    );
