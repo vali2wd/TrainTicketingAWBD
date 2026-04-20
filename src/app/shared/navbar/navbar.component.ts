d55254eafc04a419fc7ee56d5622aa2dea1ef25b
vali2wd <vali_nafo@yahoo.com>
2026-04-20 18:20:47 +0300

    Added logout functionality. Imported icon, Created button.

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 import {
   bootstrapList,
@@ -9,13 +10,16 @@ import {
   bootstrapTrainFront,
   bootstrapQuestion,
   bootstrapMegaphone,
+  bootstrapDoorClosed,
   bootstrapNewspaper,
 } from '@ng-icons/bootstrap-icons';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgIcon, MatButtonModule, MatMenuModule, RouterLink],
+  imports: [NgIcon, MatButtonModule, MatMenuModule, RouterLink, MatDividerModule, AsyncPipe],
   providers: [
     provideIcons({
       bootstrapList,
@@ -24,6 +28,7 @@ import { RouterLink } from '@angular/router';
       bootstrapQuestion,
       bootstrapNewspaper,
       bootstrapMegaphone,
+      bootstrapDoorClosed
     }),
   ],
   templateUrl: './navbar.component.html',
@@ -31,4 +36,9 @@ import { RouterLink } from '@angular/router';
 })
 export class NavbarComponent {
   showFiller = false;
+  public authService = inject(AuthService);
+
+  onLogout() {
+    this.authService.logout();
+  }
 }
